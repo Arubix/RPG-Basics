7abdfdde12acf5cbc2bfe660e8f1e8e0b767f4db
Havana <havana@Havanas-Mac.local>
2013-02-02 22:44:36 -0500

    Initial Commit

---
+++ rpg/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  rpg
+//
+//  Created by Havana on 2/2/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
