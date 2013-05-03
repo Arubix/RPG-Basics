c45025e890f1f755af5474e4bfa1ce2d7820ebbb
arubix <qbinb01@gmail.com>
2013-05-02 23:24:55 -0400

    Revert "RPG Basics"
    This reverts commit 34c01017d81701ecb185123b16bdf9aa24f2e001.

--- rpg/HelloWorldLayer.h
+++ rpg/HelloWorldLayer.h
@@ -13,21 +13,11 @@
 #import "cocos2d.h"
 
 // HelloWorldLayer
-@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>{
-    CCTMXTiledMap *theMap;
-    CCTMXLayer *bglayer;
-    CCSprite *robot;
-    CCTMXLayer *stLayer;
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
 }
-
-@property(nonatomic, retain) CCTMXTiledMap *theMap;
-@property(nonatomic, retain) CCTMXLayer *bglayer;
-@property(nonatomic, retain) CCSprite *robot;
-@property(nonatomic, retain) CCTMXLayer *stLayer;
 
 // returns a CCScene that contains the HelloWorldLayer as the only child
 +(CCScene *) scene;
--(void)setPlayerPosition:(CGPoint)position;
--(void)setCenterOfScreen:(CGPoint) position;
--(CGPoint)tileCoordForPosition:(CGPoint)position;
+
 @end
